7b9f5c23bf3cea366ab8c9d22179527d96d41feb
Arya Parvizi <aresparvizi@gmail.com>
2021-03-03 01:45:05 +0330

    initial implementation idea

--- Main.java
+++ Main.java
@@ -1,5 +1,50 @@
+import java.util.*;
 public class Main{
 	public static void main (String [] args){
 		System.out.println("Hello world!");
+	}
+
+
+	public static initializeRestaurant(String [] args){
+		if(args.length>0){
+
+		}
+		else{
+			for(int i=0; i<2; ++i){
+
+				String name = "Ramsy Gordon";
+				Map<Ingredient,Integer> reqIng =  new HashMap<Ingredient,Integer>(); // required ingredients.
+				ingredients.add("")...
+				Chef ramsy = new Chef(name, reqIng);
+			}
+		}
+	}
+}
+
+class AssistantChef{
+	
+	
+	AssistantChef(Set<String> sandwhichIngs){
+
+	}
+}
+
+class Chef{
+	String name;
+	private final Map<Ingredient, Integer> reqNoIng = new HashSet<String>(); // required number of ingredients.
+
+	Chef(String name, ingredients){
+
+	}
+
+}
+
+class Ingredient{ // CS
+	final static Map<Ingredient, Integer> giveIngGetCount = new HashMap<String, Integer>(); // give ingredient's name and get the number of available ingredients.	
+	String name;
+
+	@override
+	hash(){
+		return name.hash();
 	}
 }
